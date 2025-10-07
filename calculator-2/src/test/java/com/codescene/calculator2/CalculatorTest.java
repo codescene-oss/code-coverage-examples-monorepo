982c05960642cdf4a07b3268598c0c88d6aa5fef
Juraj Martinka <jumarko@gmail.com>
2025-10-07 11:05:44 +0200

    Proper package structure for calculator-2 and calculator-3.
    The whole point of doing this is to avoid weird edge cases in codescene
    coverage calculation where it seems to be having issues with subpaths,
    that are only added after the coverage files are parsed.
    
    This is an unusual corner case anyway and something that shouldn't exist
    under normal circumstances (you are supposed to have unique package names in Java)

--- calculator-2/src/test/java/com/codescene/calculator2/CalculatorTest.java
+++ calculator-2/src/test/java/com/codescene/calculator2/CalculatorTest.java
@@ -1,4 +1,4 @@
-package com.codescene;
+package com.codescene.calculator2;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
--- calculator-3/src/test/java/com/codescene/calculator3/CalculatorTest.java
+++ calculator-3/src/test/java/com/codescene/calculator3/CalculatorTest.java
@@ -1,4 +1,4 @@
-package com.codescene;
+package com.codescene.calculator3;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
